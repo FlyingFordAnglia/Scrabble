b6c3354fe5e40a3b89447555a2d0d160a8a58e04
FlyingFordAnglia <47173623+FlyingFordAnglia@users.noreply.github.com>
2019-03-26 20:40:09 +0530

    Update modify.py

--- code/modify.py
+++ code/modify.py
@@ -7,14 +7,18 @@ l1 = dict(zip(string.ascii_uppercase, list(range(0, 26, 1))))
 l2 = dict(zip(string.ascii_lowercase, list(range(26, 52, 1))))
 letternumberkey = {**l1, **l2}
 letternumberkey[' '] = 52
+l3 = dict(zip(list(range(0, 26, 1)), string.ascii_uppercase))
+l4 = dict(zip(list(range(26, 52, 1)), string.ascii_lowercase))
+numberletterkey = {**l3, **l4}
+numberletterkey[52] = ' '
 
-# arguments: letters is a list of string characters which you want to place on the board, 
+# arguments: letters is a list of numbers representing string characters which you want to place on the board, 
 # positions is a list of lists with each sublist containing two numbers indicating the position of the 
 # corresponding letters on the board array, player is the player making the move.
 
 def move(letters, positions, player, mainboard):
     board = mainboard.copy()
     for i in range(len(letters)):
-        board[positions[i][0], positions[i][1]] = letternumberkey[letters[i]]
-        player.rack.remove(letters[i])
+        board[positions[i][0], positions[i][1]] = letters[i]
+        player.rack.remove(numberletterkey[letters[i]])
     return board
