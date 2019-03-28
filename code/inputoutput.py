bddf1a6a049b303e73bab781a963dcf661556fc4
FlyingFordAnglia <47173623+FlyingFordAnglia@users.noreply.github.com>
2019-03-28 12:23:19 +0530

    Update inputoutput.py

--- code/inputoutput.py
+++ code/inputoutput.py
@@ -18,28 +18,32 @@ else:
         for i in range(n):
             playernames.append(input('Enter the name of player ' + str(i) + ': '))
         currentgame = GameEngine(playernames, 'new')
-        cont = False  # A bool allowing to decide for continuing based on the validity of the enetered move
-        while not cont:
-            printer(currentgame.board)
-            for i in currentgame.players:
-                print(i.name, ': Your score is ', i.score)
-            print(playernames[currentgame.turn], ', it is your turn. Here are the letters in you rack.')
-            print(currentgame.players[currentgame.turn].rack)
-            print('Type the word you want to place on the board. This should contain all the new tiles you are adding.')
-            print('Enter all non-blank tiles as capitals \
-            and any blank tiles using the small letter of the letter you want to use the blank tile as.')
-            playerinput = []
-            playerinput.append(str(input('Word to be played: ')))
-            print('Where is the first letter of the word you made on the board?')
-            playerinput.append([int(input('Enter the row number here: ')), int(input('Enter the column number here: '))])
-            print('Is your word vertically placed or horizontally placed?')
-            playerinput.append(str(input('Enter v or h here: ')))
-            if currentgame.scrabbleit(playerinput):
-                cont = True
-                reply = input('Enter Q to quit without saving, S to save the game and anything else to continue: ')
-                if reply == 'S':
-                    currentgame.save(filename=input('Enter the name of the saved file (press enter to use the default): '))
-                elif reply == 'Q':
-                    exit()
-            else:
-                print('The word/indices you entered was invalid. Please enter again.')
+        reply = 'C'
+        while reply == 'C':
+            cont = False  # A bool allowing to decide for continuing based on the validity of the enetered move
+            while not cont:
+                printer(currentgame.board)
+                for i in currentgame.players:
+                    print(i.name, ': Your score is ', i.score)
+                print(playernames[currentgame.turn], ', it is your turn. Here are the letters in you rack.')
+                print(currentgame.players[currentgame.turn].rack)
+                print('Type the word you want to place on the board. This should contain all the new tiles you are adding.')
+                print('Enter all non-blank tiles as capitals \
+                and any blank tiles using the small letter of the letter you want to use the blank tile as.')
+                playerinput = []
+                playerinput.append(str(input('Word to be played: ')))
+                print('Where is the first letter of the word you made on the board?')
+                playerinput.append([int(input('Enter the row number here: ')), int(input('Enter the column number here: '))])
+                print('Is your word vertically placed or horizontally placed?')
+                playerinput.append(str(input('Enter v or h here: ')))
+                if currentgame.scrabbleit(playerinput):
+                    cont = True
+                    reply = input('Enter Q to quit without saving, S to save the game and anything else to continue: ')
+                    if reply == 'S':
+                        currentgame.save(filename=input('Enter the name of the saved file (press enter to use the default): '))
+                    elif reply == 'Q':
+                        exit()
+                    else:
+                        reply = 'C'
+                else:
+                    print('The word/indices you entered was invalid. Please enter again.')
