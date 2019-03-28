41decd55fb9f52a3e69de5cf766d5be4b8f5b742
FlyingFordAnglia <47173623+FlyingFordAnglia@users.noreply.github.com>
2019-03-28 20:21:25 +0530

    Update GameEngine.py

--- code/GameEngine.py
+++ code/GameEngine.py
@@ -3,6 +3,10 @@ from pouch import pouch
 from savegame import savegame, loadgame
 from gamerulechecker import mainrules
 import numpy as np
+from scorer import scorer
+from modify import move
+
+
 class GameEngine(object):
 
     def __init__(self, playernames=None, gamestatus='new', validity_mode=True, filename='wordlist/sowpods.txt'):
@@ -31,7 +35,7 @@ class GameEngine(object):
     def scrabbleit(self, playerinput):
         validity = mainrules(playerinput, self.board, validity=self.validitymode, filename=self.filename)   # verifying validity of move
         if validity[0]:
-            self.players[self.turn].score += scorer(validity[1], board)  # updating score of player
+            self.players[self.turn].score += scorer(validity[1], validity[2], self.board)  # updating score of player
             self.board = move(validity[1][0], validity[1][1], self.players[self.turn], self.board)  # updating board
             self.pouch.pick(self.players[self.turn])  # picking new tiles
             self.turn += 1
